d16a155292922aa06e1ec4fd04f574bb8708c0ea
andrew giessel <andrew.giessel@gmail.com>
2012-05-31 17:44:15 -0400

    transRoutines.py
    re-write of findLevels() and addition of findLevels1d() and
    findLevelsNd()

--- traces/traceRoutines.py
+++ traces/traceRoutines.py
@@ -90,33 +90,59 @@ def findLevels(A, level, mode='rising', boxWidth=0, rangeSubset=None):
     if boxWidth is not 0:
         A = np.convolve(A, np.array([1]*boxWidth)/float(boxWidth))
 
-    rising = []
-    falling = []
-    both = []
+    crossings = np.diff(np.sign(A-level), axis=0)
     
-    for point in range(len(A)-1):
-        thisPointValue, nextPointValue =  A[point], A[point+1]
-
-        if thisPointValue < level and nextPointValue >= level:
-            risingInterpFunction = interp1d(np.array([thisPointValue, nextPointValue]), np.array([point, point+1]))
-            interpPosition = risingInterpFunction(level)
-
-            rising.append(interpPosition)
-            both.append(interpPosition)
-            
-        if thisPointValue > level and nextPointValue <= level:
-            fallingInterpFunction = interp1d(np.array([nextPointValue, thisPointValue]), np.array([point+1, point]))
-            interpPosition = fallingInterpFunction(level)
-
-            falling.append(interpPosition)
-            both.append(interpPosition)
-
     if mode is 'rising':
-        return np.array(rising), len(rising)
+        rising_points = np.where(crossings > 0)
+        return rising_points[0], len(rising_points[0])
     elif mode is 'falling':
-        return np.array(falling), len(falling)
+        falling_points = np.where(crossings < 0)
+        return falling_points[0], len(falling_points[0])
     else:
-        return np.array(both), len(both)
+        all_crossing_points = np.where(np.abs(crossings) > 0)
+        return all_crossing_points, len(all_crossing_points)
+
+
+def findLevels1d(A, level, mode='rising', boxWidth=0):
+    return findLevelsNd(A, level, mode='rising', axis=0, boxWidth=0)
+
+def findLevelsNd(A, level, mode='rising', axis=0, boxWidth=0):
+    """Function to find level crossings in an Nd numpy array. 
+
+    Can find rising and/or falling crossings, control with the 'mode' paramter.
+
+    Returns a binary array of level crossings, with true elements right AFTER a crossing.
+
+    NOTE THAT THIS RETURNS DIFFERENT VALUES THAN findLevels().  if you want to get a list of
+    locations where the crossings occurs, then use the following syntax:
+
+    levels = findLevelsNd(array, level)
+    level_crossings_locations = levels.nonzero()
+    number_of_level_crossings = len(level_crossing_locations[0])
+
+    Often, the crossings are noisy.  You can use np.diff() and findLevelsNd() again to help yourself out.
+
+
+    :param A: 1d numpy array
+    :param level: floating point to search for in A
+    :param mode: optional string: mode specfication. one of 'rising', 'falling' or 'both'
+    :param axis: optional integer, specifies dimension
+    :param boxWidth: optional int for local boxcar smoothing
+    :returns: binary array of level crossing locations
+    """
+    assert mode in ('rising', 'falling', 'both'), 'traceManip.findLevels: Unknown mode \'%s\'' % mode
+
+    if boxWidth is not 0:
+        A = nd.convolve1d(A, np.array([1]*boxWidth)/float(boxWidth), axis=axis)
+
+    crossings = np.diff(np.sign(A-level), axis=axis)
+    
+    if mode is 'rising':
+        return crossings>0
+    elif mode is 'falling':
+        return crossings<0
+    else:
+        return np.abs(crossings>0)
     
 def subBaseline(A):
     X = correctionMatrixSmoothed(A)
