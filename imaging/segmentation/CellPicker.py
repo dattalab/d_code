1221bbafc496d72102ced59ec78fa432b9339f5a
andrew giessel <andrew.giessel@gmail.com>
2013-01-28 17:01:27 -0500

    removing utility functions for PCA and correlation
    and leaving the info panel blank for now

--- imaging/segmentation/CellPicker.py
+++ imaging/segmentation/CellPicker.py
@@ -243,25 +243,6 @@ class CellPickerGUI(object):
     def clearModeData(self):
         self.modeData = []
 
-    def makeGaussian(self, size, fwhm=3, center=None):
-        """ Make a square gaussian kernel.
-        
-        size is the length of a side of the square
-        fwhm is full-width-half-maximum, which
-        can be thought of as an effective radius.
-        """
- 
-        x = np.arange(0, size, 1, float)
-        y = x[:,np.newaxis]
-    
-        if center is None:
-            x0 = y0 = size // 2
-        else:
-            x0 = center[0]
-            y0 = center[1]
-    
-        return np.exp(-4*np.log(2) * ((x-x0)**2 + (y-y0)**2) / fwhm**2)
-
 
     def lastROI(self):
         if len(self.listOfMasks) is 0:
@@ -284,7 +265,7 @@ class CellPickerGUI(object):
         assert(np.any(ROI_mask))
         return ROI_mask
 
-        
+
     def timeCourseROI(self, ROI_mask):
         if self.series is not None:
             nPixels=np.sum(ROI_mask)
@@ -292,140 +273,8 @@ class CellPickerGUI(object):
 
 
     def updateInfoPanel(self, ROI_number=None):
-        plt.figure('Info Panel')
+        pass
 
-        ROI = self.mask_from_ROI_number(ROI_number)
-        ROI = ROI.astype('bool')
-
-        pc_spatial_masks, pc_time_courses, skews, corrs, sub_region_shape = self.pcaROI(ROI_number)
-        #corr_matrix = self.correlateROI()
-        trace = self.timeCourseROI(ROI)
-
-        for i in range(4):
-            plt.subplot2grid((3,4),(0,i))
-            plt.imshow(pc_spatial_masks[:,i].reshape((sub_region_shape[0], sub_region_shape[1])), vmax=pc_spatial_masks[:,0:3].max())
-
-        # plot local correlation window
-        plt.subplot2grid((3,4),(1,0))
-        plt.plot()
-
-        # plot mask highlight
-        plt.subplot2grid((3,4),(1,3))
-        plt.imshow(self.currentMask + ROI)
-
-        # plot time series
-        plt.subplot2grid((3,4),(2,0), colspan=4)
-        plt.plot(self.timeCourseROI(ROI))
-        plt.xlim(0, self.series.shape[2])
-
-
-    def pcaROI(self, ROI_number=None):
-        print 'pca'
-        sys.stdout.flush()
-        
-        ROI = self.mask_from_ROI_number(ROI_number)
-        ROI = ROI.astype('bool')
-
-        # build correlation matrix
-        xmin = np.where(ROI)[0].min() - self.diskSize
-        xmax = np.where(ROI)[0].max() + self.diskSize
-        ymin = np.where(ROI)[1].min() - self.diskSize
-        ymax = np.where(ROI)[1].max() + self.diskSize
-
-        temp = self.series[xmin:xmax, ymin:ymax, :]
-        temp = temp.astype('float') / np.atleast_3d(temp.mean(axis=2)) - 1.0
-        temp = temp - temp.mean()
-        
-        flat = np.zeros((temp.shape[0]*temp.shape[1], temp.shape[2]))
-        for frame in range(temp.shape[2]):
-            flat[:,frame] = temp[:,:,frame].flatten()
-        flat = (flat.T - flat.mean(axis=1)).T
-
-        pca = decomposition.PCA()
-        pca.fit(flat)
-        Flat = pca.transform(flat)
-        
-        timeCourse = self.timeCourseROI(ROI)
-        timeCourse_mean = timeCourse / timeCourse.max()
-
-        # find best pca spatial mask
-        corr_list = []
-        skew_list = []
-        for i in range(10):
-            corr_list.append(np.correlate(timeCourse_mean, pca.components_[i,:]))  # maybe temporally smooth, too
-            skew_list.append(( skew(np.abs(Flat[:,i])), skew(pca.components_[i,:]), np.sqrt( skew(np.abs(Flat[:,i]))**2 + skew(pca.components_[i,:])**2 )  ))
-
-        # set global variables
-        return Flat, pca.components_, skew_list, corr_list, temp.shape
-
-        # should probably be component 0 if the cell is active and there is a limited field of view
-
-        # normalized and threshold correlation matrix
-        #norm_corr_matrix = corr_matrix / corr_matrix.max()
-        #thresholded_corr_matrix = norm_corr_matrix > self.contrastThreshold
-        # set corr matrix to zero where other ROIs exist
-        #thresholded_corr_matrix[self.currentMask.astype('bool')] = False
-        #thresholded_corr_matrix[lastROI] = True
-        
-        # open and close to remove holes
-        #thresholded_corr_matrix = pymorph.close(pymorph.open(thresholded_corr_matrix))
-
-        # update mask and redisplay
-        #newMask = np.logical_or(thresholded_corr_matrix, self.currentMask).astype(int)
-
-        #self.listOfMasks.append(newMask.copy())
-        #self.currentMask = self.listOfMasks[-1]
-
-    def correlateLastROI(self):
-        print 'corr'
-        sys.stdout.flush()
-
-        if len(self.listOfMasks) is 0:
-            print 'no mask!'
-            return None
-        elif len(self.listOfMasks) is 1:
-            print 'only 1 ROI'
-            lastROI = self.currentMask.copy()
-        else:
-            lastROI = np.logical_xor(self.listOfMasks[-1], self.listOfMasks[-2]) 
-        
-        corr_matrix = np.zeros_like(lastROI, dtype=float)
-
-        # build correlation matrix
-        xmin = np.where(lastROI)[0].min()
-        xmax = np.where(lastROI)[0].max()
-        ymin = np.where(lastROI)[1].min()
-        ymax = np.where(lastROI)[1].max()
-
-        nPixels=np.sum(lastROI)
-        timeCourse = np.sum(self.series[lastROI,:],axis=0)/nPixels
-
-        for x in range(xmin - self.diskSize * 2, xmax + self.diskSize * 2):
-            for y in range(ymin - self.diskSize * 2, ymax + self.diskSize * 2):
-                corr_matrix[x,y] = np.correlate(smooth(timeCourse, window_len=3), smooth(self.series[x,y,:], window_len=3))  # smoothing?
-
-        plt.figure()
-        plt.imshow(corr_matrix)
-
-        # normalized and threshold correlation matrix
-        norm_corr_matrix = corr_matrix / corr_matrix.max()
-        thresholded_corr_matrix = norm_corr_matrix > self.contrastThreshold
-        # set corr matrix to zero where other ROIs exist
-        thresholded_corr_matrix[self.currentMask.astype('bool')] = False
-        thresholded_corr_matrix[lastROI] = True
-        
-        # multiply with gaussian falloff
-
-        # open and close to remove holes
-        thresholded_corr_matrix = pymorph.close(pymorph.open(thresholded_corr_matrix))
-
-        # update mask and redisplay
-        newMask = np.logical_or(thresholded_corr_matrix, self.currentMask).astype(float)
-        newMask = newMask.astype('uint16')
-
-        self.listOfMasks.append(newMask.copy())
-        self.currentMask = self.listOfMasks[-1]
-        self.makeNewMaskAndBackgroundImage()
 
     def mask_from_points(self, vertex_list, size_x, size_y):
         #poly_verts = [(20,0), (50,50), (0,75)]
