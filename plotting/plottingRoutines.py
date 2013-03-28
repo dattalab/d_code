98e238ad3affed1af62a5c339c1284060113eb69
andrew giessel <andrew.giessel@gmail.com>
2013-03-28 11:25:17 -0400

    plot routine changes - added plot_array
    plot_array best used to display all traces in a 2d array in separate
    subplots (a grid)

--- plotting/plottingRoutines.py
+++ plotting/plottingRoutines.py
@@ -2,7 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as stats
 
-__all__ = ['plotSubsetOfTraceArray', 'plot_mean_and_sem']
+from mpl_toolkits.axes_grid1 import ImageGrid
+
+
+__all__ = ['plotSubsetOfTraceArray', 'plot_mean_and_sem', 'plot_array']
 
 def plotSubsetOfTraceArray(npArray, subList):
     plt.figure()
@@ -18,3 +21,27 @@ def plot_mean_and_sem(array, axis=1):
     plt.fill_between(np.arange(mean.shape[0]), sem_plus, sem_minus, alpha=0.5)
     plt.plot(mean)
 
+def plot_array(array, axis=1, plot_xlim=None, plot_ylim=None):
+
+    plt.figure()
+
+    num_plots = array.shape[axis]
+    side = np.ceil(np.sqrt(num_plots))
+    for current_plot in range(1, num_plots+1):
+
+        subplot(side, side, current_plot)
+    
+        # need to make a tuple of Ellipses and an int that is the current plot number
+        slice_obj = []
+        for a in range(array.ndim):
+            if a is axis:
+                slice_obj.append(current_plot-1)
+            else:
+                slice_obj.append(Ellipsis)
+
+        plot(array[tuple(slice_obj)])
+
+        if plot_xlim is not None:
+            xlim(plot_xlim)
+        if plot_ylim is not None:
+            ylim(plot_ylim)
