729f9ae2457164493f986f99e85487aa2e683cc0
andrew giessel <andrew.giessel@gmail.com>
2013-03-28 12:00:05 -0400

    more plotting routine changes
    renamed plot_subset_of_array and changed it to used masked arrays
    
    changed array to npArray throughout all parameters

--- plotting/plottingRoutines.py
+++ plotting/plottingRoutines.py
@@ -5,27 +5,38 @@ import scipy.stats as stats
 from mpl_toolkits.axes_grid1 import ImageGrid
 
 
-__all__ = ['plotSubsetOfTraceArray', 'plot_mean_and_sem', 'plot_array', 'imshow_array']
+__all__ = ['plot_subset_of_array', 'plot_mean_and_sem', 'plot_array', 'imshow_array', 'plot_avg_and_comps']
 
-def plotSubsetOfTraceArray(npArray, subList):
+def plot_subset_of_array(npArray, keep_slices, axis=1):
     plt.figure()
-    for i in subList:
-        plt.plot(npArray[:,i])
+    
+    mask = np.ones_like(npArray, dtype=bool)
+    for s in keep_slices:
+        # need to make a tuple of Ellipses and an int that is the current trace number
+        slice_obj = []
+        for a in range(npArray.ndim):
+            if a is axis:
+                slice_obj.append(s)
+            else:
+                slice_obj.append(Ellipsis)
+        mask[tuple(slice_obj)] = False
 
-def plot_mean_and_sem(array, axis=1):
-    mean = array.mean(axis=axis)
-    sem_plus = mean + stats.sem(array, axis=axis)
-    sem_minus = mean - stats.sem(array, axis=axis)
+    plot(ma.masked_array(npArray, mask))
+
+def plot_mean_and_sem(npArray, axis=1):
+    mean = npArray.mean(axis=axis)
+    sem_plus = mean + stats.sem(npArray, axis=axis)
+    sem_minus = mean - stats.sem(npArray, axis=axis)
     
     plt.figure()
     plt.fill_between(np.arange(mean.shape[0]), sem_plus, sem_minus, alpha=0.5)
     plt.plot(mean)
 
-def plot_array(array, axis=1, xlim=None, ylim=None):
+def plot_array(npArray, axis=1, xlim=None, ylim=None):
 
     plt.figure()
 
-    num_plots = array.shape[axis]
+    num_plots = npArray.shape[axis]
     side = np.ceil(np.sqrt(num_plots))
     for current_plot in range(1, num_plots+1):
 
@@ -33,24 +44,24 @@ def plot_array(array, axis=1, xlim=None, ylim=None):
     
         # need to make a tuple of Ellipses and an int that is the current plot number
         slice_obj = []
-        for a in range(array.ndim):
+        for a in range(npArray.ndim):
             if a is axis:
                 slice_obj.append(current_plot-1)
             else:
                 slice_obj.append(Ellipsis)
 
-        plot(array[tuple(slice_obj)])
+        plot(npArray[tuple(slice_obj)])
 
         if xlim is not None:
             plt.xlim(xlim)
         if ylim is not None:
             plt.ylim(ylim)
 
-def imshow_array(array, axis=2, vmax=None, vmin=None):
+def imshow_array(npArray, axis=2, vmax=None, vmin=None):
 
     plt.figure()
 
-    num_plots = array.shape[axis]
+    num_plots = npArray.shape[axis]
     side = np.ceil(np.sqrt(num_plots))
     for current_plot in range(1, num_plots+1):
 
@@ -58,11 +69,16 @@ def imshow_array(array, axis=2, vmax=None, vmin=None):
     
         # need to make a tuple of Ellipses and an int that is the current plot number
         slice_obj = []
-        for a in range(array.ndim):
+        for a in range(npArray.ndim):
             if a is axis:
                 slice_obj.append(current_plot-1)
             else:
                 slice_obj.append(Ellipsis)
 
-        imshow(array[tuple(slice_obj)], vmax=vmax, vmin=vmin)
+        imshow(npArray[tuple(slice_obj)], vmax=vmax, vmin=vmin)
 
+
+def plot_avg_and_comps(npArray, axis=1):
+    plt.figure()
+    plt.plot(npArray, alpha=0.25, lw=1, color='black')
+    plt.plot(npArray.mean(axis=axis), lw=2, color='black')
