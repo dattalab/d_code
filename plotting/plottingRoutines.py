fa5fc7998f8087536d87ed5fd2155fa0be496d9e
andrew giessel <andrew.giessel@gmail.com>
2013-03-28 11:36:19 -0400

    plotting routine changes
    edited arguements for plot_array()
    
    added imshow_array()

--- plotting/plottingRoutines.py
+++ plotting/plottingRoutines.py
@@ -21,7 +21,7 @@ def plot_mean_and_sem(array, axis=1):
     plt.fill_between(np.arange(mean.shape[0]), sem_plus, sem_minus, alpha=0.5)
     plt.plot(mean)
 
-def plot_array(array, axis=1, plot_xlim=None, plot_ylim=None):
+def plot_array(array, axis=1, xlim=None, ylim=None):
 
     plt.figure()
 
@@ -41,7 +41,28 @@ def plot_array(array, axis=1, plot_xlim=None, plot_ylim=None):
 
         plot(array[tuple(slice_obj)])
 
-        if plot_xlim is not None:
-            xlim(plot_xlim)
-        if plot_ylim is not None:
-            ylim(plot_ylim)
+        if xlim is not None:
+            plt.xlim(xlim)
+        if ylim is not None:
+            plt.ylim(ylim)
+
+def imshow_array(array, axis=2, vmax=None, vmin=None):
+
+    plt.figure()
+
+    num_plots = array.shape[axis]
+    side = np.ceil(np.sqrt(num_plots))
+    for current_plot in range(1, num_plots+1):
+
+        subplot(side, side, current_plot)
+    
+        # need to make a tuple of Ellipses and an int that is the current plot number
+        slice_obj = []
+        for a in range(array.ndim):
+            if a is axis:
+                slice_obj.append(current_plot-1)
+            else:
+                slice_obj.append(Ellipsis)
+
+        imshow(array[tuple(slice_obj)], vmax=vmax, vmin=vmin)
+
